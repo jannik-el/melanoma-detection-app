8f87b296a67491d214b5f94c3fc71dfffc11e098
Jannik <93831356+jannik-el@users.noreply.github.com>
2022-04-03 14:48:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def start_page():
 
     with col2:
         melanoma_image = "https://upload.wikimedia.org/wikipedia/commons/6/6c/Melanoma.jpg"
-        st.image(melanoma_image, caption='Melanoma on a patients skin, https://en.wikipedia.org/wiki/Melanoma#/media/File:Melanoma.jpg')
+        st.image(melanoma_image, caption='Melanoma on a patients skin, https://en.wikipedia.org/wiki/Melanoma#/media/File:Melanoma.jpg', width=400)
 
     return
 
